12a7497e94667fca7feb38be16c045f047a9debc
Neriol <neriol@163.com>
2024-08-12 14:27:30 +0800

    complete content's style

--- src/renderer/src/components/Content.tsx
+++ src/renderer/src/components/Content.tsx
@@ -1,7 +1,18 @@
+import { mockContent } from '@renderer/mock/content'
+import testsvg from '@renderer/assets/electron.svg'
+
 export const Content = () => {
   return (
-    <main className='p-3 bg-gray-200'>
-      11
+    <main className="p-3 bg-gray-200">
+      {mockContent.map((item) => (
+        <section key={item.id} className="h-12 py-0 flex gap-2 items-center">
+          <img src={testsvg} className='w-7 h-7 bg-contain' />
+          <div className='flex flex-col '>
+            <span className='text-base leading-6'>{item.title}</span>
+            {item?.content?.trim() !== '' && <span className='text-sm leading-3 text-stone-500'>{item.content}</span>} 
+          </div>
+        </section>
+      ))}
     </main>
   )
 }
---
+++ src/renderer/src/mock/content.ts
@@ -0,0 +1,13 @@
+type ContentItem = {
+  id: number,
+  title: string,
+  content?: string,
+  icon?: string
+}
+
+export const mockContent: ContentItem[] = [
+  {id:1, title:'Title 1', content:'Content 1'},
+  {id:2, title:'Title 2', content:'Content 2'},
+  {id:3, title:'Title 3', content:''},
+  {id:4, title:'Title 4',},
+]
